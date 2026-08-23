3c47bb2e7f22f290e10690fc500c4ab5ebaa3731
Rio266 <aksharalok12@gmail.com>
2026-08-23 11:43:21 +0100

    Add files via upload

--- sprite.py
+++ sprite.py
@@ -2,6 +2,9 @@ import pygame
 pygame.init()
 screen = pygame.display.set_mode((600, 600))
 pygame.display.set_caption("Sprite")
+fuel_level = 100
+font = pygame.font.SysFont("Arial", 20)
+text = font.render("The Rocket has Run out of Fuel.", True, "green")
 run = True
 class Sprite(pygame.sprite.Sprite):
     def __init__(self):
@@ -10,6 +13,7 @@ class Sprite(pygame.sprite.Sprite):
         self.image = pygame.transform.scale(self.image, (75, 75))
         self.rect = self.image.get_rect()
     def update(self, keys_pressed):
+        global fuel_level
         if self.rect.left < 0:
             self.rect.left = 0
         if self.rect.right > 600:
@@ -18,14 +22,18 @@ class Sprite(pygame.sprite.Sprite):
             self.rect.top = 0
         if self.rect.bottom > 600:
             self.rect.bottom = 600
-        if keys_pressed[pygame.K_w]:
-            self.rect.y -= 20
-        if keys_pressed[pygame.K_s]:
-            self.rect.y += 20
-        if keys_pressed[pygame.K_a]:
-            self.rect.x -= 20
-        if keys_pressed[pygame.K_d]:
-            self.rect.x += 20
+        if fuel_level > 0:
+            if keys_pressed[pygame.K_w]:
+                self.rect.y -= 20
+            if keys_pressed[pygame.K_s]:
+                self.rect.y += 20
+            if keys_pressed[pygame.K_a]:
+                self.rect.x -= 20
+            if keys_pressed[pygame.K_d]:
+                self.rect.x += 20
+            fuel_level -= 5
+        else:
+            screen.blit(text, (180, 250))
 group = pygame.sprite.Group()
 obj = Sprite()
 group.add(obj)
